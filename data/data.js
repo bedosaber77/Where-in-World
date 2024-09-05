95c3c864df5419d2443d43416e8a3a51d81f1e9e
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2024-09-05 23:48:37 +0300

    Refactor data filtering logic in data.js

--- data/data.js
+++ data/data.js
@@ -1,23 +1,22 @@
 import { LoadData } from "../services/loadData.js";
 
-let data = ([].concat(await LoadData()));
+let data = [].concat(await LoadData());
 
 export function getData() {
-    return data;
+  return data;
 }
 export async function updateData(region, name) {
-    var search = new RegExp(name, 'ig');
-    region = region[0].toUpperCase() + region.slice(1);
-    data = ([].concat(await LoadData()));
-    if (region != "All") {
-        data = data.filter(country => {
-            return (country.region === region && country.name.match(search));
-        });
-    }
-    else {
-        data = data.filter(country => {
-            return (country.name.match(search));
-        });
-    }
-    return data;
+  var search = new RegExp(name, "ig");
+  region = region[0].toUpperCase() + region.slice(1);
+  data = [].concat(await LoadData());
+  if (region != "All") {
+    data = data.filter((country) => {
+      return country.region === region && country.name.match(search);
+    });
+  } else {
+    data = data.filter((country) => {
+      return country.name.match(search);
+    });
+  }
+  return data;
 }
